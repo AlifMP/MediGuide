2b867ecd47ed02dab590cb5dc9915fddf710ba55
Alif Maulana <alifmaulana899@outlook.com>
2024-02-15 23:08:38 +0700

    add css, layouts, partials, modified: home

---
+++ resources/views/dashboard/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.homeLay')
+
+@section('contents')
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <a href="/" class="logo-medic"><img src="../assets/mediguide.png" alt="logo_picture"></a>
+
+    <ul class="navlist">
+        <li><a href="/" class="active">Beranda</a></li>
+        <li><a href="/blogs">Info Kesehatan</a></li>
+        <li><a href="/chat">Chat Dokter</a></li>
+        <li><a href="/login">Masuk / Daftar</a></li>
+    </ul>
+</header>
