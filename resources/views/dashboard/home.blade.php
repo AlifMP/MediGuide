e4c7e3b165454b0ce9bad3684449a42d38982bfa
Alif Maulana <alifmaulana899@outlook.com>
2024-02-19 09:04:32 +0700

    add button

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -4,6 +4,7 @@
     <section class="home" id="home">
         <div class="home-content">
             <h1 class="title">Panduan Kesehatan untuk Kehidupan yang Lebih Baik</h1>
+            <button>Chat Dokter</button>
         </div>
     </section>
 @endsection
