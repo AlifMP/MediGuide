69279671513631bdcb95bea02bc90525b2541e9f
Alif Maulana <alifmaulana899@outlook.com>
2024-02-19 09:25:03 +0700

    LANG=C git -c color.status=false status \ | sed -n -r -e '1,/Changes to be committed:/ d' \             -e '1,1 d' \             -e '/^Untracked files:/,$ d' \             -e 's/^\s*//' \             -e '/./p' \ | git commit -F -

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -4,7 +4,11 @@
     <section class="home" id="home">
         <div class="home-content">
             <h1 class="title">Panduan Kesehatan untuk Kehidupan yang Lebih Baik</h1>
-            <button>Chat Dokter</button>
+            @guest
+                <button onclick="location.href='/login'">Chat Dokter</button>
+            @else
+                <button>Chat Dokter</button>
+            @endguest
         </div>
     </section>
 @endsection
